f0adeee1a90bfefe4d9caeb0eae6c58239894e25
yamunanavada <yamunanavada@gmail.com>
2017-12-07 17:01:05 -0500

    added final changes to delete

--- lib/ViewMyTrips.rb
+++ lib/ViewMyTrips.rb
@@ -13,7 +13,7 @@ class ViewMyTrips
 
   def go
     view_trips #looks up user's trips, prints out
-    book_a_flight? #either ends in flight booked, or nothing; either way, nil
+    menu_for_view_trips #either ends in flight booked, or nothing; either way, nil
   end
 
   def view_trips
@@ -27,25 +27,51 @@ class ViewMyTrips
     end
   end
 
+  def menu_for_view_trips
+    puts "Please select an option.
+    1 - Book a Trip
+    2 - Delete a Trip
+    3 - Back to Main Menu"
+    answer = gets.chomp.to_i
+    case answer
+    when 1
+      puts "--------------------------------------"
+      book_a_flight?
+    when 2
+      puts "--------------------------------------"
+      delete_a_trip?
+    when 3
+    end
+  end
+
+
+  def delete_a_trip?
+    puts "Please list the number of the flight you would like to delete from your saved trips."
+    trip = gets.chomp.to_i
+    delete_trip(trip, @user)
+  end
+
+
+  def delete_trip(trip_to_delete, user)
+    #array of flights, find the trip, and delete it.
+    Trip.where(flight_id: user.trips[trip_to_delete - 1][:flight_id]).delete_all
+    @user.trips.reload
+
+  end
+
+
+
   def book_a_flight?
     puts "--------------------------------------"
-    puts "Would you like to book a flight? Y/N"
-    answer = gets.chomp.downcase
-    if answer == "y"
-      puts "Please list the number of the flight you would like to book. Please list just one flight."
-      flight = gets.chomp.to_i
-      book_trip(flight, @user)
-    elsif answer == "n"
-      puts "Ok, you have not selected a flight to book. Here's the menu of options."
-    else
-      puts "That was an incorrect response! Try again!"
-      book_a_flight?
-    end
+    puts "Please list the number of the flight you would like to book. Please list just one flight."
+    flight = gets.chomp.to_i
+    book_trip(flight, @user)
   end
 
 
   def book_trip(flight_to_book, user)
     flight = Flight.find_by(id: user.trips[flight_to_book - 1][:flight_id])
+
     user.trips[flight_to_book - 1].booked_flight = true
     if flight[:time_of_departure] == "-"
       puts "Congrats! You have booked your trip from #{flight[:origin]} to #{flight[:destination]} on #{flight[:date_of_departure]}. Happy traveling!"
